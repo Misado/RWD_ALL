447be025a76ee23ecf03fa86bab64a480ad430be
Ado Lin <syaorando@gmail.com>
2019-04-13 15:38:02 +0800

    改banner前備份

--- hw_final_RWD/js/all.js
+++ hw_final_RWD/js/all.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
+	// Initialize Swiper
+	var swiper = new Swiper('.swiper-container', {
+		slidesPerView: 3,
+		spaceBetween: 5,
+		loop: true,
+		autoplay: {
+			delay: 5000,
+		},
+	});
+	/*$(".swiper-slide,.swiper-slide-active,.swiper-slide-prev,.swiper-slide-next").css("margin-right","0").css("margin","20px 1%");
+	$(".swiper-slide,.swiper-slide-active,.swiper-slide-prev,.swiper-slide-next").css("width","31.33333%");*/
+
 	
 });
