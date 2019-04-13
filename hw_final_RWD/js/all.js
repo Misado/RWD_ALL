1c40e3a33e635c44bc4699a57f8639e1c970a6f5
Ado Lin <syaorando@gmail.com>
2019-04-13 21:04:23 +0800

    修改mobile版V1

--- hw_final_RWD/js/all.js
+++ hw_final_RWD/js/all.js
@@ -4,14 +4,44 @@ $(document).ready(function() {
 		slidesPerView: 3,
 		spaceBetween: 5,
 		loop: true,
-		autoplay: {
+		/*autoplay: {
 			delay: 5000,
-		},
+		},*/
+		breakpoints: {
+	    // when window width is <= 320px
+	    374: {
+	      slidesPerView: 1,
+	      spaceBetween: 0
+	    },
+	    // when window width is <= 480px
+	    /*480: {
+	      slidesPerView: 2,
+	      spaceBetween: 5
+	    },*/
+	    // when window width is <= 640px
+	    640: {
+	      slidesPerView: 2,
+	      spaceBetween: 5
+	    }
+	  }
 	});
 
 	// Banner Cycle setting
 	$(".banner").cycle({
 		fx: 'fade',
 		speed: 3000,
+	});
+
+	$(".showmenu").click(function(event) {
+		event.preventDefault();
+		/* Act on the event */
+		$("body").toggleClass('mobile');
+	});
+
+	// Back to top of the page
+	$(".backtotopBtn").click(function(event) {
+		//remove link event
+		event.preventDefault();
+		$("html,body").animate({scrollTop:0}, 1000);
 	});
 });
---
+++ js/all.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	// Initialize Swiper
+	var swiper = new Swiper('.swiper-container', {
+		slidesPerView: 3,
+		spaceBetween: 5,
+		loop: true,
+		/*autoplay: {
+			delay: 5000,
+		},*/
+	});
+
+	// Banner Cycle setting
+	$(".banner").cycle({
+		fx: 'fade',
+		speed: 3000,
+		autoplay: false;
+	});
+
+	$(".bannerMobile img").attr({
+		src: 'img/TTY_First.png',
+	});
+});
