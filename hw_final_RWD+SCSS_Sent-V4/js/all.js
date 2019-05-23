1af968c397592e9ac91ceae5708e11aabad758ce
Ado Lin <syaorando@gmail.com>
2019-05-23 21:01:55 +0800

    RWD V4

---
+++ hw_final_RWD+SCSS_Sent-V4/js/all.js
@@ -0,0 +1,119 @@
+$(document).ready(function() {
+	// Initialize Swiper
+	var swiper = new Swiper('.swiper-container', {
+		slidesPerView: 3,
+		spaceBetween: 5,
+		loop: true,
+		autoplay: {
+			delay: 5000,
+		},
+		breakpoints: {
+	    // when window width is <= 414px
+	    414: {
+	      slidesPerView: 1,
+	      spaceBetween: 0
+	    },
+	    // when window width is <= 480px
+	    /*480: {
+	      slidesPerView: 2,
+	      spaceBetween: 5
+	    },*/
+	    // when window width is <= 640px
+	    640: {
+	      slidesPerView: 2,
+	      spaceBetween: 5
+	    }
+	  }
+	});
+
+	// Banner Cycle setting
+	$(".banner").cycle({
+		fx: 'fade',
+		speed: 3000,
+	});
+
+	// navbar 1stmenu setting
+	$(".mainMenu > a").click(function(event) {
+		event.preventDefault();
+		// submenu slideToggle
+		$(this).parent().find("ul").slideToggle();
+		$(this).toggleClass('active');
+		// other submenu slideUp
+		$(this).parent().siblings().find("ul").slideUp();
+		$(this).parent().siblings().find("a").removeClass('active');
+	});
+
+	// navbar 2ndmenu setting
+	$(".subMenu a").click(function(event) {
+		$(this).toggleClass('active');
+		$(this).parent().siblings().find("a").removeClass('active');
+	});
+
+	/* hamburger menu setting */
+	$(".showmenu").click(function(event) {
+		event.preventDefault();
+		$("body").toggleClass('mobile');
+	});
+
+	// Back to top of the page
+	$(".backtotopBtn").click(function(event) {
+		event.preventDefault();
+		$("html,body").animate({scrollTop:0}, 500);
+	});
+
+	// 空心愛心跟實心愛心互換
+	$(".cuisineHeart").click(function(event) {
+		$(this).find("i").toggleClass('far fa-heart').toggleClass('fas fa-heart');
+	});
+
+	// 會員註冊條款 setting
+	$(".registerRuleMobile a").click(function(event) {
+		event.preventDefault();
+		$(".registerRuleMobile p").slideToggle();
+	});
+
+	$(".chefInfoReadMore").click(function(event){
+		event.preventDefault();
+		$(".chefSubInfo").slideToggle();
+	});
+
+	// 閱讀更多 slideToggle是直接改在html上，觸發過的話CSS的樣式吃不到，所以要用JS處理
+	window.onresize = function(){
+		let windowWidth = window.innerWidth;
+		// 視窗寬度<=768 閱讀更多要顯示，子資訊不要顯示
+		if ( windowWidth <= 768 ){
+			$(".chefSubInfo").css("display","none");
+			$(".chefInfoReadMore").css("display","block");
+		} else{
+			$(".chefSubInfo").css("display","block");
+			$(".chefInfoReadMore").css("display","none");
+		}
+	}
+
+	// 回上層的按鈕等到滑到banner下才出現
+	$(window).scroll(function(event) {
+		let scrollPos = $(window).scrollTop();
+		
+		let bannerShow = $(".banner").css("display");
+		let bannderMobileShow = $(".bannerMobile").css("display");
+
+		let bannerBottom = 0;
+		if ( bannerShow === "none" ){
+			let bannerMobileHeight = $(".bannerMobile").height()/2;
+			let bannerMobileTop = $(".bannerMobile").offset().top;
+			bannerBottom = bannerMobileTop+bannerMobileHeight;
+		} else if ( bannderMobileShow === "none" ){
+			let bannerHeight = $(".banner").height()/2;
+			let bannerTop = $(".banner").offset().top;
+			bannerBottom = bannerTop+bannerHeight;
+		}
+
+		if ( scrollPos >= bannerBottom ){
+			$(".backtotopBtn").show();
+		} else {
+			$(".backtotopBtn").hide();
+		}
+
+	});
+});
+
